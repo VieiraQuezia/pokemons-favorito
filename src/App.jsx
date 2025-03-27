141e596ff21c04195dc2cef6b8f8c6344b4e9c1d
Guih <ggsantana18@gmail.com>
2025-03-27 13:50:01 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import CorBotao from './Components/CorBotao'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home"
+import Nav from "./components/navBarra"
+import Favoritos from "./pages/favoritos"
+import CorBotao from "./Components/CorBotao"
+
 
 function App() {
-
   return (
-    <>
-    <CorBotao/>
-    </>
-  )
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoritos" element={<Favoritos/>} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
 }
 
-export default App
+export default App;
