a89c19307652f7f46dbaea222191e4d4e0fb9053
Quezia Vieira <vieiraquezia24@gmail.com>
2025-03-27 14:02:38 -0300

    nada

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,24 +1,10 @@
 import "./header.css"
-// import { useEffect, useState } from "react";
 
 function Header() {
-    //  const [pokemonsFavoritos, setPokemonsFavoritos] = useState(null);
-    
-    //   useEffect(() => {
-
-    //     const savedFavoritos = localStorage.getItem("Pokemons-Favoritos");
-    //     if (savedFavoritos) {
-    //       setPokemonsFavoritos(JSON.parse(savedFavoritos));
-    //     }
-    //   }, []);
-    
-    //   if (!pokemonsFavoritos) {
-    //     return <h2>Nenhuma informação de país disponível.</h2>;
-    //   }
     return(
         <>
             <header className="headerBox">
-                <img src="public/blinkiesCafe-7j.gif" alt="Pokémon" />
+                <img src="/blinkiesCafe-7j.gif" alt="Pokémon" />
             </header>
         </>
     )
